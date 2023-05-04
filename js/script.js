16f1ab3ee4d2ab22ef14b20c2fe17b4bf6497f1b
RolexxTime <dindjipierre77@gmail.com>
2023-05-04 23:07:03 +0200

    loader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+// LOADER
+
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})
+
 // ANIMATION TYPING
 
 var typed = new Typed(".typing",{
